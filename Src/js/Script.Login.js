d016f91dcf76d906172eb9034c3f054395016e97
jonas Tillmann Junior <jonastillmann19@gmail.com>
2024-07-07 00:04:15 -0300

    telaEntrada_FD

--- Src/js/Script.Login.js
+++ Src/js/Script.Login.js
@@ -1,3 +1,20 @@
+let password = document.getElementById("inputSenha")
+let imgeye = document.getElementById("eyes")
+function eyeopen(){
+    if (password.type == "password"){
+        password.type = "text"
+        imgeye.src = "/imgs/eye-open.png"
+    }else{
+        password.type = "password"
+        imgeye.src = "/imgs/eye-close.png"
+    }
+}
+/*
+eye-close.png
+
+eyeopen.onclick = function(){
+    
+}*/
 function btnentrar() {
     alert("funfo o botão de entrar")
 }
