712f0691b320b86187881236ec07c2269a26ed9e
jonas Tillmann Junior <jonastillmann19@gmail.com>
2024-08-04 17:42:38 -0300

    Sla 2,0

--- Src/js/Cadrastro.js
+++ Src/js/Cadrastro.js
@@ -1,14 +1,15 @@
 let password = document.getElementById("inputSenha")
+let password2 = document.getElementById("inputSenha2")
 let imgeye = document.getElementById("eyes")
+
 function eyeopen(){
     if (password.type == "password"){
         password.type = "text"
+        password2.type = "text"
         imgeye.src = "/imgs/eye-open.png"
     }else{
         password.type = "password"
+        password2.type = "password"
         imgeye.src = "/imgs/eye-close.png"
     }
-}
-function btnentrar() {
-    alert("funfo o botão de entrar")
 }
---
+++ Src/js/Login.js
@@ -0,0 +1,17 @@
+let password = document.getElementById("inputSenha")
+let imgeye = document.getElementById("eyes")
+
+function eyeopen(){
+    if (password.type == "password"){
+        password.type = "text"
+        imgeye.src = "/imgs/eye-open.png"
+    }else{
+        password.type = "password"
+        imgeye.src = "/imgs/eye-close.png"
+    }
+}
+
+/*
+function btnentrar() {
+    alert("funfo o botão de entrar")
+}*/
